ba199a3af2077dec48471f28fc16252d59d4a3e0
Vladislav-1 <44432412+Vladislav-1@users.noreply.github.com>
2025-09-18 15:10:18 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 /* -- Переменные -- */
 const mainButtonElement = document.querySelector(".main__button");
 const mainTextElement = document.querySelector(".main__text");
@@ -20,9 +19,27 @@ mainButtonElement.addEventListener(
 
     const mainSelectElement = document.getElementById("main__select").value;
     let y = mainSelectElement;
+    let z;
+    let w;
     for (let i = y; i >= 0; i--) {
       setTimeout(() => {
-        if (y > 9) {
+        if (y >= 60) {
+          z = Math.floor(y / 60);
+          w = y - 60 * z;
+          if (z > 9 && w > 9) {
+            mainTimer.innerHTML = `${z}:${w}`;
+          }
+          if (z > 9 && w <= 9) {
+            mainTimer.innerHTML = `${z}:0${w}`;
+          }
+          if (z <= 9 && w > 9) {
+            mainTimer.innerHTML = `0${z}:${w}`;
+          }
+          if (z <= 9 && w <= 9) {
+            mainTimer.innerHTML = `0${z}:0${w}`;
+          }
+        }
+        if (y > 9 && y < 60) {
           mainTimer.innerHTML = `00:${y}`;
         }
         if (y <= 9) {
@@ -38,4 +55,3 @@ mainButtonElement.addEventListener(
     );
   })
 );
-
