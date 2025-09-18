d51f6f17074038783d02cc5b8ad29d863ab86323
Vladislav-1 <44432412+Vladislav-1@users.noreply.github.com>
2025-09-18 13:49:49 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+
 /* -- Переменные -- */
 const mainButtonElement = document.querySelector(".main__button");
 const mainTextElement = document.querySelector(".main__text");
+const mainTimer = document.querySelector(".main__timer");
+
 let time;
 
 /* -- Функция работает при клике по кнопке "Старт" -- */
@@ -9,13 +12,30 @@ mainButtonElement.addEventListener(
   "click",
   (startCookingTimer = (time, showMessage) => {
     mainTextElement.innerHTML = ``;
+
     function showMessage() {
       const dishName = document.querySelector(".main__inputDishName").value;
-      mainTextElement.innerHTML = `Ваше блюдо "${dishName}" готово!<br>Приятного аппетита!`;
+      mainTextElement.innerHTML = `Ваше блюдо &laquo;<b>${dishName}</b>&raquo; готово!<br>Приятного аппетита!`;
     }
+
+    const mainSelectElement = document.getElementById("main__select").value;
+    let y = mainSelectElement;
+    for (let i = y; i >= 0; i--) {
+      setTimeout(() => {
+        if (y > 9) {
+          mainTimer.innerHTML = `00:${y}`;
+        }
+        if (y <= 9) {
+          mainTimer.innerHTML = `00:0${y}`;
+        }
+        y = y - 1;
+      }, 1000 * i);
+    }
+
     setTimeout(
       showMessage,
-      (time = document.querySelector(".main__select").value)
+      (time = document.getElementById("main__select").value * 1000)
     );
   })
 );
+
